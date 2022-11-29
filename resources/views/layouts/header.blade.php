c70be7df43f49dc683b40474fa934f9f10a36389
kowsalyaMM <96189576+kowsalyaMM@users.noreply.github.com>
2022-11-29 18:50:54 +0530

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,14 +1,16 @@
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel</title>
+  <title>Hotel Comparator</title>
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-
+ 
+  
   <!-- Bootstrap -->
   <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
 
@@ -18,6 +20,9 @@
   <!-- select -->
   <link rel="stylesheet" href="{{asset('select/css/select2.css')}}"/>
 
+  <!-- datapicker -->
+  <link rel="stylesheet" type="text/css" href="{{asset('datapicker/css/daterangepicker.css')}}" />
+    
   <!-- owl -->
   <link rel="stylesheet" href="{{asset('owl/css/owl.carousel.min.css')}}">
   <link rel="stylesheet" href="{{asset('owl/css/owl.theme.default.min.css')}}">        
@@ -30,6 +35,10 @@
      <span class="navbar-toggler-icon"></span>
   </button>
   <div id="mySidenav" class="sidenav">
+    <?php $all_data=session()->all(); 
+    $token=Auth::id();
+  //  dd(Auth::id());
+    ?>
     <ul class="navbar-nav">
     	<li><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a></li>
       <li class="nav-item">
@@ -48,13 +57,14 @@
           <option data-img_src="{{asset('images/coins/USD.svg')}}">USD</option>
         </select>
       </li>
+      @if(($login !==2))
       <li class="nav-item login">
         <a class="nav-link" id="loginbutton">Login</a>
         <div class="login-link">@include('auth/login')</div>
       </li> 
-         
-    </ul>
-    <!-- <li class="nav-item login-after">
+     @endif
+     @if(($login==2))
+      <li class="nav-item login-after" >
         <div class="dropdown">
           <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
             <img src="{{asset('images/profile.svg')}}">   
@@ -69,13 +79,31 @@
             <div class="logout-sec">
               <div class="log-out">
                 <img src="{{asset('images/logout.svg')}}">   
-                <p>Logout</p>
+                <a href="{{ route('logout') }}">Logout</a>
               </div>
             </div>
           </div>
         </div>
-      </li>   -->
+      </li> 
+      @endif   
+
+    </ul>
+     
   </div>  
 </nav>
 
+<!-- 
+<script type="text/javascript">
+  console.log('session',window.location.href);
+  var href=window.location.href;
+  var split=href.split('/');
+  console.log('href',split[3]);
+  if(split[3]=='hotels'){
+    // alert('hii');
+    $(".login-after").removeAttr("style")
+    // $('.login-after').css('display':'block');
+  }
+  // $('.login-after').css('display':'block');
+
+</script> -->
 
